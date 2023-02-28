c209e9699c07355e895c1e90bd354941471963df
Kyle Wehrung <kyle.wehrung@gmail.com>
2023-02-27 18:43:01 -0700

    lots of words going on here

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,16 @@
-# your code goes here!
+
+class Anagram:
+
+    def __init__(self, word):
+        self.word_dog = sorted([letter for letter in word])
+        
+
+    def match(self, word_list):
+        match_word_list = []
+
+        for word in word_list:
+            if sorted([letter for letter in word]) == self.word_dog:
+                match_word_list.append(word)
+
+        return match_word_list
+
